7d516d196ec0b944db20d5ac821cfdd624288891
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-09-25 23:21:15 +0700

    ✨  Create Call Video and Call

---
+++ src/main/java/com/anonymity/topictalks/configs/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.anonymity.topictalks.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author de140172 - author
+ * @version 1.1 - version of software
+ * - Package Name: com.anonymity.topictalks.configs
+ * - Created At: 25-09-2023 18:03:18
+ * @since 1.0 - version of class
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+}
