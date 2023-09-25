fa2c8a134301136a430c8677fcd5ebd08ed042e2
Le Quang Long <lqlong2001official@gmail.com>
2023-09-25 23:55:50 +0700

    fix query

--- src/main/java/com/anonymity/topictalks/daos/message/IParticipantRepository.java
+++ src/main/java/com/anonymity/topictalks/daos/message/IParticipantRepository.java
@@ -27,7 +27,7 @@ public interface IParticipantRepository extends IBaseRepository<ParticipantPO, P
 
     List<ParticipantPO> findAllByUserInfo(UserPO userPO);
 
-    @Query(value = "SELECT a.user_id FROM participant a, conversation b WHERE b.conversation_id= :conversation_id AND a.user_id != :user_id", nativeQuery = true)
+    @Query(value = "SELECT a.user_id FROM participant a, conversation b WHERE b.conversation_id= :conversation_id AND a.conversation_id= :conversation_id AND a.user_id != :user_id", nativeQuery = true)
     long getPartnerIdByConversationIdAndUserId(@Param(value = "conversation_id") long conversation_id, @Param(value = "user_id") long user_id );
 
 }
