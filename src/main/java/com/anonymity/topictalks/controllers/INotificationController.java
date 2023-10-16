c9dad79fe6574b4e795f3e72ef98b7e6a891e28a
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-10-16 22:43:29 +0700

    ✨  Creating Message Notification and get all of them.

---
+++ src/main/java/com/anonymity/topictalks/controllers/INotificationController.java
@@ -0,0 +1,38 @@
+package com.anonymity.topictalks.controllers;
+
+import com.anonymity.topictalks.models.payloads.requests.NotiRequest;
+import com.anonymity.topictalks.services.INotificationService;
+import com.anonymity.topictalks.utils.commons.ResponseData;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author de140172 - author
+ * @version 1.2 - version of software
+ * - Package Name: com.anonymity.topictalks.controllers
+ * - Created At: 16-10-2023 22:32:24
+ * @since 1.0 - version of class
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notification")
+@PreAuthorize("hasAnyRole('ADMIN','USER')")
+@Tag(name = "MessageNoti", description = "The Notification API")
+public class INotificationController {
+
+    private INotificationService notificationService;
+
+    @PostMapping("/pushNoti")
+    public ResponseData saveNotiMessage(@RequestBody NotiRequest request) {
+        notificationService.saveNotiMessage(request);
+        return ResponseData.ofSuccess("success", "save notification successfully");
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseData getAllNotiByUserId(@PathVariable("userId") Long userId) {
+        return ResponseData.ofSuccess("success", notificationService.notiList(userId));
+    }
+
+}
---
+++ src/main/java/com/anonymity/topictalks/services/impls/NotificationServiceImpl.java
@@ -0,0 +1,68 @@
+package com.anonymity.topictalks.services.impls;
+
+import com.anonymity.topictalks.daos.message.IMessageRepository;
+import com.anonymity.topictalks.daos.notification.IMessageNotificationRepository;
+import com.anonymity.topictalks.daos.user.IUserRepository;
+import com.anonymity.topictalks.models.payloads.requests.NotiRequest;
+import com.anonymity.topictalks.models.persists.notification.MessageNotificationPO;
+import com.anonymity.topictalks.models.persists.notification.QMessageNotificationPO;
+import com.anonymity.topictalks.services.INotificationService;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author de140172 - author
+ * @version 1.2 - version of software
+ * - Package Name: com.anonymity.topictalks.services.impls
+ * - Created At: 16-10-2023 22:18:54
+ * @since 1.0 - version of class
+ */
+@Service
+@Slf4j
+public class NotificationServiceImpl implements INotificationService {
+
+    @Autowired
+    private IMessageNotificationRepository messageNotificationRepository;
+    @Autowired
+    private IUserRepository userRepository;
+    @Autowired
+    private IMessageRepository messageRepository;
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
+    /**
+     * @param request
+     */
+    @Override
+    public void saveNotiMessage(NotiRequest request) {
+        MessageNotificationPO noti = MessageNotificationPO
+                .builder()
+                .id(request.getId())
+                .messageId(messageRepository.findById(request.getMessageId()).orElse(null))
+                .userId(userRepository.findById(request.getUserId()).orElse(null))
+                .build();
+        messageNotificationRepository.save(noti);
+    }
+
+    /**
+     * @param userId
+     * @return
+     */
+    @Override
+    public List<MessageNotificationPO> notiList(Long userId) {
+        QMessageNotificationPO qMessageNotificationPO = QMessageNotificationPO.messageNotificationPO;
+        List<MessageNotificationPO> listNotiUser = jpaQueryFactory.select(
+                Projections.bean(qMessageNotificationPO)
+        ).from(qMessageNotificationPO)
+                .where(qMessageNotificationPO.userId.eq(userRepository.findById(userId).orElse(null)))
+                .fetch();
+        return listNotiUser;
+    }
+}
