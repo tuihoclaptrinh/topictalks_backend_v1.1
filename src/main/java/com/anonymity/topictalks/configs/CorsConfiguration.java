690df03a8ea1dcf5fb787716bed851311bc7566d
tuihoclaptrinh <105140089+tuihoclaptrinh@users.noreply.github.com>
2023-12-20 10:40:19 +0700

    ✨ Deployment.

--- src/main/java/com/anonymity/topictalks/configs/CorsConfiguration.java
+++ src/main/java/com/anonymity/topictalks/configs/CorsConfiguration.java
@@ -17,7 +17,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://topictalks.online")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
